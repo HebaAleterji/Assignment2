bab39895b9725a1b4e7f30219e0cf046a9c07e54
Heba Aleterji <hebaaleterji@gmail.com>
2024-08-06 14:58:03 +1200

    influence_diagnostics, plot functions

--- R/functions.R
+++ R/functions.R
@@ -24,3 +24,42 @@ hadis_influence_measure <- function(model) {
 }
 
 
+
+#' @title Perform Influence Diagnostics
+#' @description Computes selected influence measures for an lm model.
+#' @param data The dataset used in the model.
+#' @param model An object of class lm.
+#' @param measure The influence measure to compute ("cooks", "dffits", or "hadi").
+#' @return A numeric vector of the selected influence measure.
+#' @examples
+#' model <- lm(mpg ~ wt + hp, data = mtcars)
+#' influence_diagnostics(mtcars, model, "cooks")
+influence_diagnostics <- function(data, model, measure = c("cooks", "dffits", "hadi")) {
+  validate_inputs(data, model)
+  measure <- match.arg(measure)
+
+  result <- switch(measure,
+                   "cooks" =  cooks.distance(model),
+                   "dffits" = dffits(model),
+                   "hadi" = hadis_influence_measure(model))
+
+  plot_influence(data, model)
+
+  return(result)
+}
+
+#' @title Plot Influence Measures
+#' @description Plots the selected influence measure for an lm model.
+#' @param data The dataset used in the model.
+#' @param model An object of class lm.
+#' @param measure The influence measure to plot ("cooks", "dffits", or "hadi").
+#' @examples
+#' model <- lm(mpg ~ wt + hp, data = mtcars)
+#' plot_influence(mtcars, model, "cooks")
+plot_influence <- function(data, model, measure = "cooks") {
+  diagnostics <- influence_diagnostics(data, model, measure)
+
+  plot(diagnostics, main = paste("Influence Measure:", measure),
+       ylab = "Influence", xlab = "Index")
+}
+
